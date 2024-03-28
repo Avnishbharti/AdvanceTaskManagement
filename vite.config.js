b1ac914efe00d891e69c7a4ab90466ba2e0907d5
Avnishbharti <avnishkumar4605@gmail.com>
2024-03-28 11:29:31 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactVendor: ['react', 'react-dom'],
+          // Define additional manual chunks as needed
+        }
+      }
+    }
+  }
 })
